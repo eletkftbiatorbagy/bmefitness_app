876bd267d6994fbeb57e818416c0ca2b82e239da
Attila Kiss <attila.kiss@e-let.hu>
2014-04-25 17:18:46 +0200

    sync +

--- www/js/sync.js
+++ www/js/sync.js
@@ -1,21 +1,42 @@
+var SyncStarted=false;
+
 function Sync()
 {
-	window.requestFileSystem(window.PERSISTENT, 0, onFileSystemSuccess, fail);
+	if (SyncStarted) { return; }
+	SyncStarted=true;
 	
-	
+	callback = function(response) { GetRemoteDirs('AJAX_LOGIN',response); } 
+	ajax_hivas(AJAX_URL +'login_get.php','', 'callback' ,'AJAX_LOGIN'); 
 }
 
-function onFileSystemSuccess(fs) {
+var RemoteDirs;
+
+function GetRemoteDirs(DOMelement,response)
+{
+	if (!response) { return; }
+	RemoteDirs = JSON.parse(response);
+	FreeCallback(DOMelement);
+	
+	window.requestFileSystem(window.PERSISTENT, 0, StartScanning, fail);
+}
+
+function StartScanning(fs) 
+{
         console.log("FileSystem OK");
         fs.root.getDirectory('db', {create: true}, null, fail);
        	var dirReader = fs.root.createReader();
   		var entries = [];
   		
-  		var readEntries = function() {		console.log("readEntries - start");
-			 dirReader.readEntries (function(results) {
-			  if (!results.length) {		console.log("readEntries - Filesystem is empty.");
+  		var readEntries = function() 
+  		{		
+			 dirReader.readEntries (function(results) 
+			 {
+			  if (!results.length) 
+			  {		
 				listResults(entries.sort());
-			  } else {						console.log("readEntries - toArray");
+			  } 
+			  else 
+			  {						
 				entries = entries.concat(toArray(results));
 				readEntries();
 			  }
@@ -23,99 +44,24 @@ function onFileSystemSuccess(fs) {
 		};
 		
 		readEntries();
-    }
+}
 
 
 function listResults(entries) {
 	console.log("listResults : " + entries.length);
-  entries.forEach(function(entry, i) {
+  	entries.forEach(function(entry, i) {
    if (entry.isDirectory) { console.log("[" + entry.name + "]"); } else {console.log(entry.name);}
     
   });
 }
 
 function toArray(list) {
+	console.log("list = "+list);
   return Array.prototype.slice.call(list || [], 0);
 }
 
 
 
 
-function getDirSuccess(dirEntry)
-{
-	//console.log("dirEntry : "+dirEntry.name);
-	var directoryReader = dirEntry.createReader();
-	directoryReader.readEntries(success,fail);
-	
-}
 
-function success(entries) {
-    var i;
-    for (i=0; i<entries.length; i++) {
-        console.log(entries[i].name);
-    }
-}
-
-
-function fail(error) {
-	var desc={};
-	desc[FileError.NOT_FOUND_ERR]="File not found";
-	desc[FileError.SECURITY_ERR]="Security error";
-	desc[FileError.ABORT_ERR]="Abort error";
-	desc[FileError.NOT_READABLE_ERR]="File not readable";
-	desc[FileError.ENCODING_ERR]="File encoding error";
-	desc[FileError.NO_MODIFICATION_ALLOWED_ERR]="No file modification allowed";
-	desc[FileError.INVALID_STATE_ERR]="Invalid state";
-	desc[FileError.SYNTAX_ERR]="Syntax error";
-	desc[FileError.INVALID_MODIFICATION_ERR]="Invalid modification error";
-	desc[FileError.QUOTA_EXCEEDED_ERR]="Quota exceeded";
-	desc[FileError.TYPE_MISMATCH_ERR]="Type mismatch";
-	desc[FileError.PATH_EXISTS_ERR]="Path exists";
-	description = desc[error.code] || "Ismeretlen hiba";
-    console.log("File ERROR : " + error.code+" / " + description);
-}
-
-
-
-    
-function DirectoryReader(localURL) {
-    this.localURL = localURL || null;
-    this.hasReadEntries = false;
-}
-
-DirectoryReader.prototype.readEntries = function(successCallback, errorCallback) 
-{
-	if (this.hasReadEntries) 
-		{
-        	successCallback([]);
-        	return;
-    	}
-    var reader = this;
-    var win = typeof successCallback !== 'function' ? null : function(result) {
-        var retVal = [];
-        for (var i=0; i<result.length; i++) {
-            var entry = null;
-            if (result[i].isDirectory) {
-                entry = new (require('./DirectoryEntry'))();
-            }
-            else if (result[i].isFile) {
-                entry = new (require('./FileEntry'))();
-            }
-            entry.isDirectory = result[i].isDirectory;
-            entry.isFile = result[i].isFile;
-            entry.name = result[i].name;
-            entry.fullPath = result[i].fullPath;
-            entry.filesystem = new (require('./FileSystem'))(result[i].filesystemName);
-            entry.nativeURL = result[i].nativeURL;
-            retVal.push(entry);
-        }
-        reader.hasReadEntries = true;
-        successCallback(retVal);
-    };
-    
-	var fail = typeof errorCallback !== 'function' ? null : function(code) {
-        errorCallback(new FileError(code));
-    };
-    exec(win, fail, "File", "readEntries", [this.localURL]);
-};
 	   
