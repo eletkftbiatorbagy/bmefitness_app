75809ec936453babffce13ef3049d25586e5964f
Attila Kiss <attila.kiss@e-let.hu>
2014-04-28 15:15:34 +0200

    na nézzük

--- www/js/sync.js
+++ www/js/sync.js
@@ -1,5 +1,23 @@
 var SzinkronStart=false;
 
+function fail(error) {
+	var desc={};
+	desc[FileError.NOT_FOUND_ERR]="File not found";
+	desc[FileError.SECURITY_ERR]="Security error";
+	desc[FileError.ABORT_ERR]="Abort error";
+	desc[FileError.NOT_READABLE_ERR]="File not readable";
+	desc[FileError.ENCODING_ERR]="File encoding error";
+	desc[FileError.NO_MODIFICATION_ALLOWED_ERR]="No file modification allowed";
+	desc[FileError.INVALID_STATE_ERR]="Invalid state";
+	desc[FileError.SYNTAX_ERR]="Syntax error";
+	desc[FileError.INVALID_MODIFICATION_ERR]="Invalid modification error";
+	desc[FileError.QUOTA_EXCEEDED_ERR]="Quota exceeded";
+	desc[FileError.TYPE_MISMATCH_ERR]="Type mismatch";
+	desc[FileError.PATH_EXISTS_ERR]="Path exists";
+	description = desc[error.code] || "Ismeretlen hiba";
+    console.log("File ERROR : " + error.code+" / " + description);
+}
+
 function Szinkron()
 {
 	if (SzinkronStart) { return; }
