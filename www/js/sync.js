34824226dd5312d083515a66e778d933a16182f7
Attila Kiss <attila.kiss@e-let.hu>
2014-04-28 15:32:24 +0200

    próba

--- www/js/sync.js
+++ www/js/sync.js
@@ -66,7 +66,6 @@ function StartScanning(fs)
 
 
 function listResults(entries) {
-	console.log("listResults : " + JSON.stringify(entries));
   	entries.forEach(function(entry, i) {
    if (entry.isDirectory) { console.log("[" + entry.name + "]"); } else {console.log(entry.name);}
     
